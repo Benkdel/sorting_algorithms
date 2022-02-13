17d8d9ffb5c4085631673e3c76a04e6f6ae1c11c
Carlos Bentin <cbentin12@gmail.com>
2022-02-13 12:05:56 -0500

    reorg files

--- tests/run.sh
+++ tests/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-gcc -g -Wall -Wextra -Werror -pedantic -std=gnu89 ../print_array.c ../print_list.c $1 $2 -o a;
+gcc -g -Wall -Wextra -Werror -pedantic -std=gnu89 print_array.c print_list.c $1 $2 -o a;
 
 ./a
