52d16e18e311a4f9fdebcfc15b8a1839ff4ee1e4
Carlos Bentin <cbentin12@gmail.com>
2022-02-14 13:19:49 -0500

    removing return

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,26 +11,27 @@ void bubble_sort(int *array, size_t size)
 	unsigned int status = 0, index = 0;
 	int temp = 0;
 
-	if (size < 2)
-		return;
-	while (1)
+	if (size >= 2)
 	{
-		if ((index + 1) > size)
+		while (1)
 		{
-			if (status == index)
-				break;
-			index = 0;
-			status = 0;
+			if ((index + 1) > size)
+			{
+				if (status == index)
+					break;
+				index = 0;
+				status = 0;
+			}
+			if (array[index + 1] < array[index])
+			{
+				temp = array[index + 1];
+				array[index + 1] = array[index];
+				array[index] = temp;
+				status = 0;
+				print_array(array, size);
+			}
+			index++;
+			status++;
 		}
-		if (array[index + 1] < array[index])
-		{
-			temp = array[index + 1];
-			array[index + 1] = array[index];
-			array[index] = temp;
-			status = 0;
-			print_array(array, size);
-		}
-		index++;
-		status++;
 	}
 }
